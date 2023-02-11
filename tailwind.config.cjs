c2a689e6f6023489091f23a3e24301c2039b0e03
Matt Stenback <mstenback@gmail.com>
2023-02-11 14:17:19 -0330

    Updated to Astro 2. Added tailwind. Added fonts and colors. Started setting up the homepage layout. Added hero text with animating name.

---
+++ tailwind.config.cjs
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			'cartridge': {
+				DEFAULT: '#f2f9ff',
+				400: '#f2f9ff',
+				500: '#ced7f2',
+			},
+			'purple': '#9582d9',
+			'orange': '#f2884b',
+			'indigo': '#273858',
+		},
+		extend: {
+			fontFamily: {
+				'serif': ['swear-text', 'serif'],
+				'display': ['irregardless-variable', 'sans-serif'],
+				'mono': ['Vulf Mono Light Italic', 'monospace'],
+			},
+			fontSize: {
+				sm: '0.65rem',
+			},
+			rotate: {
+        '15': '15deg',
+      }
+		},
+	},
+	plugins: [],
+}
