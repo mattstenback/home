583cf0c857c4b1f252d62f300ccffc9144dc9ed1
Matt Stenback <mstenback@gmail.com>
2023-02-22 21:56:30 -0330

    Added an Article layout. Installed MDX. Added a couple of MDX components for Paragraph and Subheadings in articles.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
-    config: { applyBaseStyles: false },
-  })]
+    config: {
+      applyBaseStyles: false
+    }
+  }), mdx()]
 });
