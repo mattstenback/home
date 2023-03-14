02e2d2bc80558defd15e20dd525313eca104d30f
Matt Stenback <mstenback@gmail.com>
2023-03-13 23:04:21 -0230

    Added google analytics and partytown.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,13 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     config: {
       applyBaseStyles: false
     }
-  }), mdx()]
+  }), mdx(), partytown()]
 });
